91f4e164a67faa9f356cca3ecc42a0e4df9c9933
hermesboo <71751817+hermesboo@users.noreply.github.com>
2020-11-19 06:18:19 -0500

    i wanna be safe

--- src/main.c
+++ src/main.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void yuki(FILE *fp) {
+        char actread;
+
+        while ((actread = getc(fp) != EOF)) {
+                printf("%c", actread);
+        }
+}
+
+int main(int argc, char **argv) {
+        FILE *filename;
+        int argnum;
+
+        printf("Argument Count: %d\n", argc);
+        argnum = argc - 1;
+        printf("Argnum: %d\n", argnum);
+        do {
+                filename = fopen(argv[argnum], "r");
+                if (filename == NULL) {
+                        printf("Error opening files");
+                        exit(-1);
+                }
+
+                yuki(filename);
+                fclose(filename);
+                argnum++;
+                printf("EOF\n");
+        } while (argnum != argc);
+        return 0;
+}
